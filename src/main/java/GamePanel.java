afb82bc8b1447bf28d30a5710cf75890725d8994
Kamau Francis Kanyua <kamaukan77@gmail.com>
2022-08-02 03:31:31 +0300

    Created a game panel

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -21,6 +21,12 @@ public class GamePanel extends JPanel implements ActionListener {
     Random random;
 
     GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
 
     public void startGame() {
